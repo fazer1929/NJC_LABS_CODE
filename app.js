a50c1516427d165b69f3bbf28b587e493b2e71e6
fazer1929 <abhishekagrawal8888@gmail.com>
2021-09-04 16:03:40 +0530

    Port Config

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const sqlite3 = require("sqlite3");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //Middle WareSetup
 app.set("view engine", "ejs");
